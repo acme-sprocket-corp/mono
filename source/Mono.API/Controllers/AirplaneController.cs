95b25dfd60ac513be95b87399c5f991c4e0fc83b
Michael Bradvica <mjbradvica@gmail.com>
2025-06-29 10:50:31 -0400

    update build and ci/cd

--- source/Mono.API/Controllers/AirplaneController.cs
+++ source/Mono.API/Controllers/AirplaneController.cs
@@ -1,11 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
+// <copyright file="AirplaneController.cs" company="Acme Sprocket Co">
+// Copyright (c) Acme Sprocket Co. All rights reserved.
+// </copyright>
 
 namespace Mono.API.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <inheritdoc />
     [ApiController]
     [Route("airplane")]
     public class AirplaneController : ControllerBase
     {
+        /// <summary>
+        /// Returns all airplanes.
+        /// </summary>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
         [HttpGet("", Name = "GetAirplanes")]
         public IActionResult Get()
         {
--- source/Mono.API/Program.cs
+++ source/Mono.API/Program.cs
@@ -1,8 +1,18 @@
+// <copyright file="Program.cs" company="Acme Sprocket Co">
+// Copyright (c) Acme Sprocket Co. All rights reserved.
+// </copyright>
 
 namespace Mono.API
 {
+    /// <summary>
+    /// Main entry class.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main entry function.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
